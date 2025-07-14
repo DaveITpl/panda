b6e69a49d98b6009a7c786d0f66611e2599330f9
Dawid Juszczak <d.juszczak@neurosys.com>
2025-07-14 19:57:49 +0200

    fix

--- products-config.js
+++ products-config.js
@@ -5,25 +5,25 @@ const productsData = {
         products: {
             "Panda Mix Owocowy": {
                 type: "S",
-                barcode: "5905616103020",
+                barcode: "5905616103129",
                 flavor1: "ZIELONY BANAN",
                 flavor2: "WINOGRONO"
             },
             "Panda Longfill Mięta": {
                 type: "L",
-                barcode: "5905616103021",
+                barcode: "5905616103129",
                 flavor1: "MIĘTA",
                 flavor2: "LODOWA"
             },
             "Panda Sól Truskawka": {
                 type: "S",
-                barcode: "5905616103022",
+                barcode: "5905616103129",
                 flavor1: "TRUSKAWKA",
                 flavor2: "ŚMIETANKA"
             },
             "Panda Longfill Wanilia": {
                 type: "L",
-                barcode: "5905616103023",
+                barcode: "5905616103129",
                 flavor1: "WANILIA",
                 flavor2: "KARMEL"
             }
@@ -34,25 +34,25 @@ const productsData = {
         products: {
             "Siciliana Limoncello": {
                 type: "L",
-                barcode: "5905616103030",
+                barcode: "5905616103129",
                 flavor1: "LIMONCELLO",
                 flavor2: "CYTRYNA"
             },
             "Siciliana Sól Cytryna": {
                 type: "S",
-                barcode: "5905616103031",
+                barcode: "5905616103129",
                 flavor1: "CYTRYNA",
                 flavor2: "BAZYLIA"
             },
             "Siciliana Tiramisu": {
                 type: "L",
-                barcode: "5905616103032",
+                barcode: "5905616103129",
                 flavor1: "TIRAMISU",
                 flavor2: "KAWA"
             },
             "Siciliana Sól Bazylia": {
                 type: "S",
-                barcode: "5905616103033",
+                barcode: "5905616103129",
                 flavor1: "BAZYLIA",
                 flavor2: "POMIDOR"
             }
@@ -63,31 +63,31 @@ const productsData = {
         products: {
             "Grygusiowa Jabłko": {
                 type: "L",
-                barcode: "5905616103040",
+                barcode: "5905616103129",
                 flavor1: "JABŁKO",
                 flavor2: "CYNAMON"
             },
             "Grygusiowa Sól Gruszka": {
                 type: "S",
-                barcode: "5905616103041",
+                barcode: "5905616103129",
                 flavor1: "GRUSZKA",
                 flavor2: "WANILIA"
             },
             "Grygusiowa Malina": {
                 type: "L",
-                barcode: "5905616103042",
+                barcode: "5905616103129",
                 flavor1: "MALINA",
                 flavor2: "JEŻYNA"
             },
             "Grygusiowa Sól Wiśnia": {
                 type: "S",
-                barcode: "5905616103043",
+                barcode: "5905616103129",
                 flavor1: "WIŚNIA",
                 flavor2: "MIGDAŁ"
             },
             "Grygusiowa Sól Jagoda": {
                 type: "S",
-                barcode: "5905616103043",
+                barcode: "5905616103129",
                 flavor1: "JAGODA",
                 flavor2: "MIGDAŁ"
             }
